1c172bc5487dde5672930fe4c630e9f996ea1de0
Pajo <pajohack@gmail.com>
2018-04-02 17:47:46 +0000

    Initial Commit

---
+++ resources/views/createlisting.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+
+@section('contents')
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create Listing</div>
+
+                <div class="panel-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
