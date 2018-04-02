75238e0a90274f953715678099313bf4831f934a
Pajo <pajohack@gmail.com>
2018-04-02 18:07:22 +0000

    Form components created and registered in boot method

--- resources/views/createlisting.blade.php
+++ resources/views/createlisting.blade.php
@@ -9,7 +9,15 @@
                 <div class="panel-heading">Create Listing</div>
 
                 <div class="panel-body">
-                    
+                    {!!Form::open(['action' => 'ListingsController@store','method' => 'POST'])!!}
+                    {{Form::bsText('name','',['placeholder' => 'Company Name'])}}
+                    {{Form::bsText('website','',['placeholder' => 'Company Website'])}}
+                    {{Form::bsText('email','',['placeholder' => 'Contact Email'])}}
+                    {{Form::bsText('phone','',['placeholder' => 'Contact Phone'])}}
+                    {{Form::bsText('address','',['placeholder' => 'Business Address'])}}
+                    {{Form::bsTextArea('bio','',['placeholder' => 'About This Business'])}}
+                    {{Form::bsSubmit('submit')}}
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
